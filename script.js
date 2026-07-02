1c3d13a2d921872da7974a37ceb772bf813a8a75
sthefanyribeirosouza-hash <sthefany.ribeiro.souza@escola.pr.gov.br>
2026-07-01 22:44:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let pontos = 0;
+let respostas = 0;
 
+function resposta(correta){
+    respostas++;
+
+    if(correta){
+        pontos++;
+    }
+
+    if(respostas === 3){
+        document.getElementById("resultado").innerHTML =
+        "Você acertou " + pontos + " de 3 perguntas!";
+    }
+}
